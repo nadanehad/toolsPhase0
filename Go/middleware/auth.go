d4655bf584506a3bfaccf4a5521364f8c443afbc
Sara-Hammad <152811008+Sara-Hammad@users.noreply.github.com>
2024-11-09 01:24:07 +0200

    Update auth.go

--- Go/middleware/auth.go
+++ Go/middleware/auth.go
@@ -2,14 +2,16 @@ package middleware
 
 import (
 	"net/http"
+	"playlist/models"
 	"playlist/sessions"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
+func AuthMiddleware(DB *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Retrieve session_id from the cookies
+		// Retrieve the session ID from the cookie
 		sessionID, err := c.Cookie("session_id")
 		if err != nil || sessionID == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
@@ -23,13 +25,24 @@ func AuthMiddleware() gin.HandlerFunc {
 		sessions.SessionStore.RUnlock()
 
 		if !exists {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized - Invalid session ID"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
 
-		// Set the userID in the context for use in the handler functions
-		c.Set("userID", userID)
+		// Query the database to get the user and their role
+		var user models.User
+		if err := DB.First(&user, userID).Error; err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+			c.Abort()
+			return
+		}
+
+		// Set the user ID and role in the context for role-based access control
+		c.Set("userID", user.ID)
+		c.Set("role", user.Role)
+
+		// Continue to the next middleware or handler
 		c.Next()
 	}
 }
