b6f9604806fd50f233298804cabb6aff47cbc8b3
nadanehad <135297608+nadanehad@users.noreply.github.com>
2024-11-09 23:49:31 +0200

    Update orderController.go

--- Go/controllers/orderController.go
+++ Go/controllers/orderController.go
@@ -11,36 +11,34 @@ import (
 func CreateOrder(c *gin.Context) {
 	var order models.Order
 
-	// Logging the incoming JSON data
 	if err := c.ShouldBindJSON(&order); err != nil {
-		log.Println("Bind Error:", err) // Debug log for binding issue
+		log.Println("Bind Error:", err) 
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input data"})
 		return
 	}
 
 	userID, exists := c.Get("userID")
 	if !exists {
-		log.Println("Unauthorized: No user ID found") // Debug log for unauthorized user
+		log.Println("Unauthorized: No user ID found") 
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}
 
-	log.Println("User ID:", userID) // Log the User ID
+	log.Println("User ID:", userID) 
 
 	order.UserID = userID.(uint)
 
 	if result := DB.Create(&order); result.Error != nil {
-		log.Println("Database Create Error:", result.Error) // Debug log for database error
+		log.Println("Database Create Error:", result.Error) 
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create order"})
 		return
 	}
 
-	log.Println("Order created successfully for User ID:", userID) // Log successful creation
+	log.Println("Order created successfully for User ID:", userID) 
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Order created successfully", "order": order})
 }
 
-// fetch the orders for the logged in user
 
 func GetUserOrders(c *gin.Context) {
 	userID, exists := c.Get("userID")
@@ -58,7 +56,6 @@ func GetUserOrders(c *gin.Context) {
 	c.JSON(http.StatusOK, orders)
 }
 
-// info of a specific order by order ID
 func GetOrderDetails(c *gin.Context) {
 	userID, exists := c.Get("userID")
 	if !exists {
@@ -84,7 +81,6 @@ func GetOrderDetails(c *gin.Context) {
 	})
 }
 
-// Endpoint to cancel a specific order by order ID
 func CancelOrder(c *gin.Context) {
 	userID, exists := c.Get("userID")
 	if !exists {
