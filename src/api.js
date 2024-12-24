b8d44948cc1c0daf0ab2d8884ae1103bf04c9ab2
kenzi khaled <133533642+kenzzzzzzzzzz@users.noreply.github.com>
2024-12-24 19:04:38 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL ;
 
 export const registerUser = async (formData) => {
   try {
